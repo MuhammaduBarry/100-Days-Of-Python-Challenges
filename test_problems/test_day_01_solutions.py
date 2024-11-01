0f1f7a94dfe3e12a979d9e8458b3b35ccbe45e7a
Muhammadu Barry <muhammadubarry5@gmail.com>
2024-11-01 14:21:46 -0400

    Update .gitignore to ignore __pycache__ and test_problems folder

--- test_problems/test_day_01_solutions.py
+++
@@ -1,6 +0,0 @@
-from solutions.day_01_solutions import divisible_by_7_not_multiple_5
-
-def test_divisible_by_7_not_multiple_5():
-    solution = divisible_by_7_not_multiple_5()
-    expected = [number for number in range(2000, 3201) if number % 7 == 0 and number % 5 != 0]
-    assert solution == expected
